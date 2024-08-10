842c87b757745c519953c903300af81ba96bf4b4
Bradley Reynolds <bradley.reynolds@impressdesigns.com>
2024-08-10 14:21:53 +0000

    Add flowbite
    Signed-off-by: GitHub <noreply@github.com>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,14 @@
 import type { Config } from 'tailwindcss'
 
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./node_modules/flowbite/**/*.js'
+	],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		require('flowbite/plugin')
+	],
 } satisfies Config
